c5aa2e4e5b8d549157bba5bb7046925d08c033c8
maaaaaasato <final.flash.732@gmail.com>
2026-09-05 18:28:21 +0800

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script"; //これを追加
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,7 +24,22 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        {/* 以下を追加 */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FNHQ0LRZ13"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-FNHQ0LRZ13');
+  `}
+        </Script>
+      </body>
     </html>
   );
 }
